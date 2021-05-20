2c2f7a47c3c07ad061935b45eab2b97ba0993dd0
cookfisher <cookfisher@outlook.com>
2021-05-20 11:41:01 -0400

    add Newton

---
+++ ex_Newton_method.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+// the z² − x above is how far away z² is from where it needs to be (x), and the division by 2z is the derivative of z², to scale how much we adjust z by how quickly z² is changing. This general approach is called Newton's method.
+func Sqrt(x float64) float64 {
+	z := float64(x)/2
+//	z := float64(1)
+	for i:=0;i<10;i++{
+	    z -= (z*z-x)/(2*z)
+		fmt.Printf("%v ", z)
+		if d:=math.Abs(math.Sqrt(x)-z); d < 1.0e-16 {
+			fmt.Println("\ni=%v d=%v",i,d)
+		    return z
+		}
+	}
+	fmt.Println("\n",math.Sqrt(x)-z)
+	return z
+}
+
+func main() {
+	fmt.Println(Sqrt(100))
+}
